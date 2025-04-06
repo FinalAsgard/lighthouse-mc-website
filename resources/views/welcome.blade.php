30bf1d34df2c648d78f71f114138d0df7c72ce4b
vengestone-dragoon <ezekiel.littlefield@gmail.com>
2025-04-06 15:09:22 -0400

    fixed top padding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,7 +124,7 @@
         top: 0; bottom: 0; right: 0; left: 0; position: absolute;
     }
     .main-scroll-box {
-        padding-top:100px;
+        padding-top:0px;
         padding-bottom:50px;
         display: flex;
         flex-direction: column;
